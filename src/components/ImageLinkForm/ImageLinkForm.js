3ba75c4a9eecd964f54a4d98103da699f19929cc
s102034013 <xu3cj84wu0h9@gmail.com>
2019-03-27 16:33:55 -0700

    change minor text

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,14 +5,17 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
 	return (
 		<div className='ma4 mt0'>
 			<p className='f3'>
-				{'This Magic Brain will detect faces in your pictures. Git it a picture.'}
-
+				{'Detect faces in your picture. Give it a picture(URL).'}
 			</p>
-
+			<p className='f3'>
+				{'Ex. http://cseweb.ucsd.edu/~cyang/IU.jpg'}
+			</p>
+			
 			<div className='center'>
 				<div className='form center pa4 br3 shadow-5'>
 
 					<input className='f4 pa2 w-70 center' type='text' onChange={onInputChange}/>
+
 					<button 
 					className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple' onClick={onButtonSubmit}>Detect</button>
 					
